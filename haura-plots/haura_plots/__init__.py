8940bf33f99449365b71863ce147ace3dc9c9838
Johannes Wünsche <johannes@spacesnek.rocks>
2023-01-20 16:40:37 +0100

    plots: reorganize plotting scripts

--- haura-plots/haura_plots/__init__.py
+++ haura-plots/haura_plots/__init__.py
@@ -1,4 +1,4 @@
-#! /bin/env python
+#!/bin/env python
 import json
 import sys
 import numpy as np
@@ -9,145 +9,62 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.cm as cm
 import matplotlib.colors as mat_col
 import matplotlib
-    
-# Constants
-BLOCK_SIZE = 4096
-EPOCH_MS=500
-SEC_MS=1000
 
+import util
+import metrics_plots
 
-# For color reference of "Wong" color scheme see:
-# https://davidmathlogic.com/colorblind/#%23000000-%23E69F00-%2356B4E9-%23009E73-%23F0E442-%230072B2-%23D55E00-%23CC79A7
-GREEN='#009E73'
-YELLOW='#F0E442'
-BLUE='#0072B2'
-LIGHT_BLUE='#56B4E9'
-RED='#D55E00'
-ORANGE='#E69F00'
+# def plot_latency(data):
+#     epoch = [temp['epoch_ms'] for temp in data]
+#     util.subtract_first_index(epoch)
+#     fig, ax = plt.subplots(figsize=(15,5))
+#     for x in range(4):
+#         for y in range(4):
+#             lat = np.array([])
+#             for temp in data:
+#                 if x >= len(temp['storage']['tiers']) or y >= len(temp['storage']['tiers'][x]['vdevs']):
+#                     continue
+#
+#                 lat = np.append(lat, temp['storage']['tiers'][x]['vdevs'][y]['read_latency'])
+#
+#             if len(lat) > 0:
+#                 lat = lat / 100000
+#                 lat.astype(int)
+#                 lat = np.array([np.nan if elem == 0 else elem for elem in lat])
+#
+#
+#                 ax.plot(epoch, lat, label = "Average Latency {}/{}".format(x,y))
+#     fig.legend()
+#     # Epoch in seconds
+#     ms_to_string = lambda time: f"{int(time / 1000 / 60)}:{int(time / 1000) % 60:02d}"
+#     epoch_formatted = list(map(ms_to_string, epoch))
+#     ax.set_xlabel("runtime (minute:seconds)")  # add X-axis label
+#     ax.set_xticks(epoch, epoch_formatted)
+#     ax.locator_params(tight=True, nbins=10)
+#     ax.set_ylabel("Read Latency in ms")  # add Y-axis label
+#     label=' | '.join(sys.argv[1].split('/')[-2:])
+#     ax.set_title(f"Haura - {label}")  # add title
+#     fig.savefig(f"{sys.argv[1]}/plot_latency.svg")
 
-def subtract_last_index(array):
-    last_val = 0
-    for index, value in enumerate(array):
-        array[index] = value - last_val
-        last_val = value
-    array[0] = 0
-
-def subtract_first_index(array):
-    first_val = array[0]
-    for index, value in enumerate(array):
-        array[index] = value -first_val
-
-def num_to_name(tier):
-    match tier:
-        case 0:
-            return 'Fastest'
-        case 1:
-            return 'Fast'
-        case 2:
-            return 'Slow'
-        case 3:
-            return 'Slowest'
-        case _:
-            return '???'
-
-def plot_throughput(data):
-    epoch = [temp['epoch_ms'] for temp in data]
-    subtract_first_index(epoch)
-    fig, axs = plt.subplots(4, 1, figsize=(16,8))
-    colors=[GREEN, YELLOW, BLUE, RED]
-    markers=['o', '^', '']
-    for x in range(4):
-        for y in range(4):
-            writes = np.array([])
-            reads = np.array([])
-            for temp in data:
-                if x >= len(temp['storage']['tiers']) or y >= len(temp['storage']['tiers'][x]['vdevs']):
-                    continue
-
-                writes = np.append(writes, temp['storage']['tiers'][x]['vdevs'][y]['written'])
-                reads = np.append(reads, temp['storage']['tiers'][x]['vdevs'][y]['read'])
-
-            if len(writes) > 0:
-                subtract_last_index(writes)
-                subtract_last_index(reads)
-
-                # convert to MiB from Blocks
-                # NOTE: We assume here a block size of 4096 bytes as this is the default haura block size
-                # if you change this you'll need to modify this here too.
-                writes = writes * BLOCK_SIZE / 1024 / 1024 * (SEC_MS / EPOCH_MS)
-                reads = reads * BLOCK_SIZE / 1024 / 1024 * (SEC_MS / EPOCH_MS)
-                axs[x].plot(epoch, reads, label = 'Read', linestyle='dotted', color=GREEN)
-                axs[x].plot(epoch, writes, label = 'Written', color=BLUE)
-
-            ms_to_string = lambda time: f"{int(time / 1000 / 60)}:{int(time / 1000) % 60:02d}"
-            epoch_formatted = list(map(ms_to_string, epoch))
-            axs[x].set_xlabel("runtime (minute:seconds)")  # add X-axis label
-            axs[x].set_xticks(epoch, epoch_formatted)
-            axs[x].locator_params(tight=True, nbins=10)
-
-            axs[x].set_ylabel(f"{num_to_name(x)}\nMiB/s (I/0)")  # add Y-axis label
-            label=' | '.join(sys.argv[1].split('/')[-2:])
-    fig.legend(loc="center right",handles=axs[0].get_lines())
-    # Epoch in seconds
-    fig.suptitle(f"Haura - {label}", y=0.98)  # add title
-    fig.savefig(f"{sys.argv[1]}/plot_write.svg")
-    for x in range(4):
-        lines = axs[x].get_lines()
-        if len(lines) > 0:
-            lines[0].set_linestyle('solid')
-            lines[0].zorder = 2.1
-            lines[1].set_linestyle('dotted')
-            lines[1].zorder = 2.0
-    fig.legend(loc="center right",handles=axs[0].get_lines())
-    fig.savefig(f"{sys.argv[1]}/plot_read.svg")
-
-def plot_latency(data):
-    epoch = [temp['epoch_ms'] for temp in data]
-    subtract_first_index(epoch)
-    fig, ax = plt.subplots(figsize=(15,5))
-    for x in range(4):
-        for y in range(4):
-            lat = np.array([])
-            for temp in data:
-                if x >= len(temp['storage']['tiers']) or y >= len(temp['storage']['tiers'][x]['vdevs']):
-                    continue
-
-                lat = np.append(lat, temp['storage']['tiers'][x]['vdevs'][y]['read_latency'])
-
-            if len(lat) > 0:
-                lat = lat / 100000
-                lat.astype(int)
-                lat = np.array([np.nan if elem == 0 else elem for elem in lat])
-
-
-                ax.plot(epoch, lat, label = "Average Latency {}/{}".format(x,y))
-    fig.legend()
-    # Epoch in seconds
-    ms_to_string = lambda time: f"{int(time / 1000 / 60)}:{int(time / 1000) % 60:02d}"
-    epoch_formatted = list(map(ms_to_string, epoch))
-    ax.set_xlabel("runtime (minute:seconds)")  # add X-axis label
-    ax.set_xticks(epoch, epoch_formatted)
-    ax.locator_params(tight=True, nbins=10)
-    ax.set_ylabel("Read Latency in ms")  # add Y-axis label
-    label=' | '.join(sys.argv[1].split('/')[-2:])
-    ax.set_title(f"Haura - {label}")  # add title
-    fig.savefig(f"{sys.argv[1]}/plot_latency.svg")
-
-# Access string subslice and first tuple member
 def sort_by_o_id(key):
+    """
+    Access string subslice and first tuple member
+    """
     return int(key[0][2:])
 
 def plot_object_distribution():
-    fs = open(f"{sys.argv[1]}/tier_state.jsonl", 'r')
-    data = read_jsonl(fs)
-    fs.close()
+    """
+    Plot colorcoded grids to show object distribution
+    """
+    data = []
+    with open(f"{sys.argv[1]}/tier_state.jsonl", 'r', encoding='UTF-8') as state_file:
+        data = util.read_jsonl(state_file)
     colors = {
-        0: "white",
-        1: GREEN,
-        2: YELLOW,
-        3: BLUE,
-    }    
-    cmap = mat_col.ListedColormap([colors[x] for x in colors.keys()])
+        0: util.WHITE,
+        1: util.GREEN,
+        2: util.YELLOW,
+        3: util.BLUE,
+    }
+    cmap = mat_col.ListedColormap([x[1] for x in colors.items()])
     labels = np.array(["Not present", "Fastest", "Fast", "Slow"])
     num_ts = 0
     # three groups fixed
@@ -158,12 +75,12 @@ def plot_object_distribution():
         keys = []  # Vec<(key, num_tier)>
         num_tier = 1
         for tier in current_timestep:
-            for object in tier["files"]:
-                keys.append((object, num_tier))
+            for obj in tier["files"]:
+                keys.append((obj, num_tier))
             num_tier += 1
-    
+
         keys.sort(key=sort_by_o_id)
-    
+
         # old boundaries update when needed
         # seldom accessed 1-2000 (45x45)
         # barely accessed 2001-2300 (18x18)
@@ -176,7 +93,7 @@ def plot_object_distribution():
         group_1 = np.concatenate((np.array(group_1), np.zeros(4096 - len(group_1)))).reshape((64,64))
         group_2 = np.concatenate((np.array(group_2), np.zeros(676 - len(group_2)))).reshape((26,26))
         group_3 = np.concatenate((np.array(group_3), np.zeros(64 - len(group_3)))).reshape((8,8))
-    
+
         num_group = 0
         fig, axs = plt.subplots(1, 4, figsize=(20,5))
         for group in [group_1, group_2, group_3]:
@@ -191,7 +108,7 @@ def plot_object_distribution():
             subax.yaxis.set_ticks([])
             subax.xaxis.set_ticks([])
         #divider = make_axes_locatable(subax)
-        #cax = divider.append_axes("right", size="5%", pad=0.05) 
+        #cax = divider.append_axes("right", size="5%", pad=0.05)
         #fig.colorbar(im, cax=cax)
         fmt = matplotlib.ticker.FuncFormatter(lambda x, pos: labels[x])
         ticks = [0, 1, 2, 3]
@@ -203,8 +120,8 @@ def plot_object_distribution():
             num_tiers = 0
             for tier in current_timestep:
                 num_tiers += 1
-                resp_times = 0;
-                total = 0;
+                resp_times = 0
+                total = 0
                 for o_id in tier["reqs"]:
                     resps = tier["reqs"][f"{o_id}"]
                     size = tier["files"][f"{o_id}"][1]
@@ -229,9 +146,9 @@ def plot_object_distribution():
         num_ts += 1
 
     fig, ax = plt.subplots(figsize=(10,5))
-    ax.plot(mean_group_vals[0], color=ORANGE, label="Seldomly Accessed Group", marker="o", markevery=10);
-    ax.plot(mean_group_vals[1], color=LIGHT_BLUE, label="Occassionally Accessed", marker="s", markevery=10);
-    ax.plot(mean_group_vals[2], color=RED, label="Often Accessed", marker="^", markevery=10);
+    ax.plot(mean_group_vals[0], color=util.ORANGE, label="Seldomly Accessed Group", marker="o", markevery=10);
+    ax.plot(mean_group_vals[1], color=util.LIGHT_BLUE, label="Occassionally Accessed", marker="s", markevery=10);
+    ax.plot(mean_group_vals[2], color=util.RED, label="Often Accessed", marker="^", markevery=10);
     # we might want to pick the actual timestamps for this
     ax.set_xlabel("Timestep")
     ax.set_ylabel("Mean object tier")
@@ -241,6 +158,9 @@ def plot_object_distribution():
     fig.savefig(f"{sys.argv[1]}/plot_timestep_means.svg", bbox_extra_artists=(pls_no_cut_off,), bbox_inches='tight')
 
 def plot_tier_usage(data):
+    """
+    Plot the utilized space of each storage tier.
+    """
     fig, axs = plt.subplots(4, 1, figsize=(10,13))
 
     # 0 - 3; Fastest - Slowest
@@ -341,32 +261,24 @@ def plot_filesystem_test():
     fig.savefig(f"{sys.argv[1]}/filesystem_comp.svg")
 
 
-def read_jsonl(file):
+USAGE_HELP="""Please specify an input run directory. If you already completed\
+benchmarks they can be found under `results/*`.
+
+Usage:
+    haura-plots <path/to/benchmark>
+"""
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(USAGE_HELP)
+        sys.exit(2)
     data = []
-    while True:
-        # Get next line from file
-        line = file.readline()
-        # if line is empty
-        # end of file is reached
-        if not line:
-            break
-        json_object = json.loads(line)
-        data.append(json_object);
-    return data
+    with open(f"{sys.argv[1]}/betree-metrics.jsonl", 'r', encoding="UTF-8") as metrics:
+        data = util.read_jsonl(metrics)
 
-if len(sys.argv) < 2:
-    print("Please specify an input run directory. If you already completed benchmarks they can be found under `results/*`.")
-    exit(1)
-
-
-fs = open(f"{sys.argv[1]}/betree-metrics.jsonl", 'r')
-# print("{}".format(data))
-data = read_jsonl(fs)
-fs.close()
-
-# Plot actions
-plot_throughput(data)
-plot_tier_usage(data)
-#plot_latency(data)
-plot_object_distribution()
-plot_filesystem_test()
+    # Plot actions
+    metrics_plots.plot_throughput(data)
+    plot_tier_usage(data)
+    #plot_latency(data)
+    plot_object_distribution()
+    plot_filesystem_test()
